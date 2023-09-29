79e9cffba4a4c0f7279757493fdf1cb51e5eb687
Ivan Eroshin <itseroshin@gmail.com>
2023-09-30 01:57:37 +0300

    fead: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const brackets = Object.fromEntries(bracketsConfig);
+    const stack = [];
+
+    console.log(brackets);
+
+    for (const char of str) {
+        if (brackets[char]) {
+
+            if (
+                ((brackets[char] === '|') && stack.includes('|')) ||
+                ((brackets[char] === '7') && stack.includes('7')) ||
+                ((brackets[char] === '8') && stack.includes('8'))
+            ){
+                const lastOpBracket = stack.pop();
+                if (brackets[lastOpBracket] !== char) {
+                    return false;
+                }
+            } else {
+                stack.push(char);
+            }
+        } else {
+            const last = stack.pop();
+            if (brackets[last] !== char) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
 }
